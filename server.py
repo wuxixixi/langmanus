da6e59d22e7dd3c0974c376535b0a7b0636e57e4
吴曦 <wuxi_xixi@163.com>
2025-04-18 07:36:21 +0800

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.api.app:app",
         host="0.0.0.0",
-        port=8000,
+        port=8899,
         reload=True,
         log_level="info",
     )
